2405107d3b96d2c1626b8ba46c213a4687d33798
Maxson Jordan <maxson.jordan@gmail.com>
2023-03-11 19:45:24 -0300

    changes

--- api/index.ts
+++ api/index.ts
@@ -1,35 +1,131 @@
 import express, { Router, Request, Response } from 'express';
+import axios from 'axios';
+import {
+  detectBufferMime,
+} from 'mime-detect'
+
 
 const router: Router = express.Router();
 
 // Rota da API
 
 
-/**
- * @swagger
- * /api/hello:
- *   post:
- *     description: Cria um novo usuário
- *     parameters:
- *       - name: username
- *         description: Nome do usuário
- *         in: formData
- *         required: true
- *         type: string
- *       - name: email
- *         description: E-mail do usuário
- *         in: formData
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: Usuário criado com sucesso
- */
-router.get('/api/hello', (req: Request, res: Response) => {
-  
-  const name: string = req.params.name;
-  res.json({ message: `Olá, ${name}!` });
+router.post('/webhook', (req: Request, res: Response) => {
+
+  console.log("params", req.params);
+  console.log("query", req.query);
+  console.log("body", req.body);
+  console.log("headers", req.headers);
+
+  res.json({ message: `Olá!` });
 });
+
+router.post('/sendText', async (req: Request, res: Response) => {
+
+  if (!req.body.args)
+    return res.status(400).json({ success: false, message: "Erro 400: Bad Request - Parâmetros necessários não enviados! Por favor, verifique se todos os parâmetros obrigatórios foram incluídos na requisição." })
+
+  const { to, content } = req.body?.args;
+
+  await $whatsapp
+    .sendText(to, content)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((erro) => {
+      return res.status(400).json(erro); //return object error
+    });
+
+});
+
+router.post('/sendFile', async (req: Request, res: Response) => {
+
+  if (!req.body.args)
+    return res.status(400).json({ success: false, message: "Erro 400: Bad Request - Parâmetros necessários não enviados! Por favor, verifique se todos os parâmetros obrigatórios foram incluídos na requisição." })
+
+  const { to, filename, file } = req.body?.args;
+
+  const regexUrl = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+
+  let filePrepared = file;
+
+  if (file && file.match(regexUrl)) {
+    filePrepared = await getImageAsBase64(file);
+  }
+
+  await $whatsapp
+    .sendFileFromBase64(to, filePrepared, filename)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((erro) => {
+      return res.status(400).json(erro); //return object error
+    });
+
+});
+
+router.post('/sendListMenu', async (req: Request, res: Response) => {
+
+  if (!req.body.args)
+    return res.status(400).json({ success: false, message: "Erro 400: Bad Request - Parâmetros necessários não enviados! Por favor, verifique se todos os parâmetros obrigatórios foram incluídos na requisição." })
+
+  const { to, title, description, buttonText, menu } = req.body?.args;
+
+  await $whatsapp
+    .sendListMenu(to, title, description, buttonText, menu)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((erro) => {
+      return res.status(400).json(erro); //return object error
+    });
+
+});
+
+router.post('/sendButtons', async (req: Request, res: Response) => {
+
+  if (!req.body.args)
+    return res.status(400).json({ success: false, message: "Erro 400: Bad Request - Parâmetros necessários não enviados! Por favor, verifique se todos os parâmetros obrigatórios foram incluídos na requisição." })
+
+  const { to, title, description, buttons } = req.body?.args;
+
+  await $whatsapp
+    .sendButtons(to, title, buttons, description)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((erro) => {
+      return res.status(400).json(erro); //return object error
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+async function getImageAsBase64(url: string): Promise<string> {
+  const response = await axios.get(url, {
+    responseType: 'arraybuffer', // definindo o tipo de resposta como arraybuffer
+  });
+  const buffer = Buffer.from(response.data, 'binary'); // convertendo para buffer
+  const base64 = buffer.toString('base64'); // convertendo para base64
+
+
+  const type = await detectBufferMime(buffer);
+  console.log(type)
+  const prefix = `data:${type};base64,`;
+
+  return prefix + base64;
+}
+
 
 
 
